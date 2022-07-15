114edd4a7db3dbb9ba956aedcb63a564c3647c8e
Kateria Wynn <iamkateria@gmail.com>
2022-07-14 20:39:44 -0700

    added randomGame func

--- index.js
+++ index.js
@@ -28,3 +28,34 @@ function countDown(num) {
 }
 
 countDown(4);
+
+/* 
+Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75.
+*/
+
+// let counter = 0;
+// let timer = setInterval(() =>{}) w/ anon func inside
+// { select a random number between 0 and 1 Math.random() does this automatically } --> this code runs in setInterval
+// if num is greater than .75 --> clearInterval(timer)
+// console.log(counter);
+// else --> means .75 not reached --> add 1 to counter --> counter++
+
+function getRandom() {
+  return Number(Math.random().toFixed(2));
+}
+
+function randomGame() {
+  let counter = 0;
+  let timer = setInterval(() => {
+    let number = getRandom();
+    if (number > 0.75) {
+      clearInterval(timer);
+      counter++;
+      console.log(counter);
+    } else {
+      counter++;
+    }
+  }, 1000);
+}
+
+randomGame();
